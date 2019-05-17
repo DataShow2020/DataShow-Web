490dfda9c5d39e9e895408aefd4f4d4d989dda55
Ganblingbling <1096304927@qq.com>
2019-05-17 13:48:22 +0800

    11111111

--- src/view/employee/api.js
+++ src/view/employee/api.js
@@ -2,19 +2,14 @@ import {server} from '../../tools/servers'
 
 export class EmployeeApi {
   static GetTableList(inquiry) {
-    console.log("查看列表");
     return server.connection('GET', `Employee/A01`, inquiry);
   }
 
   static GetItemApi(search) {
-    console.log("查看Employee");
-    // return 2;
     return server.connection('POST', `Employee/A05`, search);
   }
 
   static AddEmployeeApi(data) {
-    console.log("新增Employee ");
-    console.log(data);
     return server.connection('POST', `Employee/A02`, data);
   }
 
